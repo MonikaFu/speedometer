591cdd82079db31bd8ac57d9e0fb52c45ce62f51
MonikaFu <monika.furdyna@gmail.com>
2021-11-22 17:14:06 +0100

    Add speedometers dashboard

--- js/initialize_charts_mini.js
+++ js/initialize_charts_mini.js
@@ -4,9 +4,11 @@ var imported;
 imported = document.createElement("script"); imported.src = "js/d3.v4.min.js"; document.head.appendChild(imported);
 
 // internal libs
-imported = document.createElement("script"); imported.src = "js/speedometer.js"; document.head.appendChild(imported);
+//imported = document.createElement("script"); imported.src = "js/speedometer.js"; document.head.appendChild(imported);
+imported = document.createElement("script"); imported.src = "js/speedometer_dashboard.js"; document.head.appendChild(imported);
 
 window.addEventListener('load', (event) => {
+	/*
 	var graph_id = "speedometer";
 	var labels = {};
 	var opts = {
@@ -14,4 +16,9 @@ window.addEventListener('load', (event) => {
 		svgWidth: 250
 	};
 	window[graph_id] = new speedometer("div[id='"+ graph_id +"']", data_speedometer, labels, opts);
+	*/
+	var graph_id = "speedometer_dashboard";
+	var labels_dashboard = {};
+	var opts_dashboard = {}
+	window[graph_id] = new speedometer_dashboard("div[id='"+ graph_id +"']", data_tdm, labels_dashboard, opts_dashboard);
 });
